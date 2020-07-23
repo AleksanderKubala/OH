ab87496cb977164aa9075b09bf306ebdd885b5e9
Aleksander Kubala <olek.kubala@wp.pl>
2020-07-23 16:32:35 +0200

    git cache cleanup

--- OHLogic/_from_unity_awaiting_revision/Actions/IActionWithData.cs
+++
@@ -1,9 +0,0 @@
-﻿using OHLogic.Data;
-using OHLogic.DataObjects;
-
-namespace Assets.Actions
-{
-    interface IActionWithData<out TData> : IAction, IObjectWithActionData<TData> where TData : IActionData
-    {
-    }
-}
